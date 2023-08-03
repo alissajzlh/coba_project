e967cc58ca38c55bc3516b7d0c4ec910ce290fd8
Alisa Nuril Jazilah <alisanuril22@gmail.com>
2023-08-03 14:21:10 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,33 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function () {
+    return view('index');
+});
+Route::prefix('home')->group(function () {
+    Route::get('/dashboard-2', function () {
+        return view('dashboard-2.index');
+    });
+    Route::get('/dashboard-3', function () {
+        return view('dashboard-3.index');
+    });
+});
+Route::prefix('auth')->group(function () {
+    Route::get('/login', function () {
+        return view('front-pages.login.index');
+    });
+    Route::get('/register', function () {
+        return view('front-pages.register.index');
+    });
+});
+Route::prefix('tables')->group(function () {
+    Route::get('/static-tables', function () {
+        return view('tables.static-tables.index');
+    });
+    Route::get('/gridjs', function () {
+        return view('tables.gridjs.index');
+    });
+    Route::get('/tabulator', function () {
+        return view('tables.tabulator.index');
+    });
+});
